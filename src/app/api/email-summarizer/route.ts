8bb3ef716ffb4148a071268c4ca2b6bea6eabb4e
rahulJalan23 <rahuljalan23.rj@gmail.com>
2024-09-09 23:18:16 +0530

    Fix: Updated the email summarizer response

--- src/app/api/email-summarizer/route.ts
+++ src/app/api/email-summarizer/route.ts
@@ -11,8 +11,19 @@ export async function POST(req: Request) {
 
   const { text } = await generateText({
     model: groq('llama-3.1-70b-versatile'),
-    system: 'You are an email summarizer.',
+    system: `You are a helpful assistant. Create a SMS summary based on the above data.
+Make sure that the summary is short. Not more than 400 characters.
+The email is send by the school to the parents of the students studying at the school.
+The aim is to take the above email data and create a brief actionable and informative SMS for the busy parents.
+
+EXAMPLE RESPONSE:
+- Picnic on Saturday, 10/23/24.
+- Make sure to pack extra lunch and evening snacks.
+- You my pack a Mat, Toys and other games as well.
+
+Return only the summary and nothing else.`,
     prompt: content,
+    maxTokens: 1024,
   });
   return new Response(JSON.stringify({ text }), {
     status: 200,
