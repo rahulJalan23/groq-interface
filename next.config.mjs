2785e47d54b2302ff75c1dfb39d4992491a69a42
rahulJalan23 <rahuljalan23.rj@gmail.com>
2024-09-12 01:21:37 +0530

    FEAT: Pdf upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ['pdf-parse', 'multer'],
+  },
+};
 
 export default nextConfig;
---
+++ src/app/api/parse-pdf/index.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from 'next/server';
+import PdfParse from 'pdf-parse';
+
+export async function POST(req: Request) {
+  try {
+    const formData = await req.formData();
+    console.log(formData);
+
+    const file = formData.get('pdf') as File;
+    const arrayBuffer = await file.arrayBuffer();
+
+    const buffer = Buffer.from(arrayBuffer);
+
+    const data = await PdfParse(buffer);
+
+    return NextResponse.json({ result: data.text }, { status: 200 });
+  } catch (e) {
+    console.error(e);
+    return NextResponse.json({ status: 'fail', error: e });
+  }
+}
